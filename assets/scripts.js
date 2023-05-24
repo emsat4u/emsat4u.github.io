478397fe387cb6e540732d30920aa71902e99a1f
moelasec <M.CS.Dev@protonmail.com>
2023-05-24 11:01:47 +0400

    🐞 moving js to script

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,28 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+// Notification 
+let notifyBar = document.getElementById('notificationBar');
+function closeNotificationBar() {
+    notificationBar.style.display = 'none';
+}
+
+let windowHeight = window.innerHeight;
+let scrollThreshold = windowHeight * 0.4;
+
+function updateNotificationBar() {
+    let scrollPosition = window.scrollY;
+
+    if (scrollPosition >= scrollThreshold) {
+        notifyBar.style.display = 'block';
+        window.removeEventListener('scroll', updateNotificationBar);
+    } else {
+        notifyBar.style.display = 'none';
+    }
+}
+
+window.addEventListener('scroll', updateNotificationBar);
+
+// Call the function initially to set the initial state of the notification bar
+updateNotificationBar();
